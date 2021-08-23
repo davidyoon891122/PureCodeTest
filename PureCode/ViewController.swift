5f7563ac8ecc6d76b16c8fd6bb47013b9698bb19
davidyoon <qqpo12@naver.com>
2021-08-23 12:57:29 +0900

    remove test button, textField and add upperView(UIView), set views on viewDidLoad with addSubview

--- PureCode/ViewController.swift
+++ PureCode/ViewController.swift
@@ -21,34 +21,11 @@ class ViewController: UIViewController {
         return imageView
     }()
     
-    
-    var testButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Button", for: .normal)
-        button.tintColor = .red
-        button.backgroundColor = .black
-        button.layer.borderColor = UIColor.white.cgColor
-        button.layer.borderWidth = 2.0
-        button.layer.cornerRadius = 20.0
-        button.clipsToBounds = true
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-    
-    lazy var testField: UITextField = {
-        let field = UITextField()
-        field.text = "i am testing"
-        field.textColor = .black
-        field.contentVerticalAlignment = .top
-        field.backgroundColor = .yellow
-        field.layer.borderWidth = 2.0
-        field.layer.borderColor = UIColor.black.cgColor
-        field.layer.cornerRadius = 0.5 * 100
-        //field.clipsToBounds = true
-        field.layer.masksToBounds = true
-        field.translatesAutoresizingMaskIntoConstraints = false
-        return field
+    lazy var upperView: UIView = {
+        let view = UIView()
+        view.autoSetDimension(.height, toSize: 128)
+        view.backgroundColor = .gray
+        return view
     }()
     
 
@@ -56,18 +33,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .blue
         // Do any additional setup after loading the view.
+        self.view.addSubview(avater)
+        self.view.addSubview(upperView)
         
-        view.addSubview(testButton)
-        view.addSubview(testField)
-        testButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
-        testButton.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 150).isActive = true
-        //testButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -50).isActive = true
-        testButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
         
-        testField.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        testField.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        testField.topAnchor.constraint(equalTo: testButton.bottomAnchor, constant: 10).isActive = true
-        testField.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
         
     }
 
