88ca08776701cc9490139543ba4093224888605b
davidyoon <qqpo12@naver.com>
2021-08-23 09:39:59 +0900

    Initial Commit

---
+++ PureCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PureCode
+//
+//  Created by David Yoon on 2021/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
